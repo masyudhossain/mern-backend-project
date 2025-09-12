6ba95bef18815fd26c37eba52813a330637095ff
Masyud Hossain <masyud.hossain2001@gmail.com>
2025-09-12 12:34:09 +0530

    connect with mongo database

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import { configDotenv } from "dotenv";
+import connectDB from "./db/dbConfig.js";
+
+configDotenv();
+connectDB()
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error;
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listing on port ${process.env.PORT}`);
+
+        })
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err;
+    }
+})()*/
