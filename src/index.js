e33b4ee8cd5e007f57271b3912c570666c88556e
Masyud Hossain <masyud.hossain2001@gmail.com>
2025-09-13 11:59:55 +0530

    upate files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import connectDB from "./db/dbConfig.js";
 
 configDotenv();
 connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error;
+        });
+        app.listen(process.env.PORT || 5000, () => {
+            console.log(`Server is running at port:
+                ${process.env.PORT}`);
+
+        });
+    })
+    .catch((err) => {
+        console.log("Mongo db connection failed!! ", err)
+    });
 
 
 
